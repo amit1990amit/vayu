ad8ba6954b711f6cb512ec41a80db927e48714b1
amit1990amit <abelamit8@gmail.com>
2024-11-20 18:02:26 +0200

    starter router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
